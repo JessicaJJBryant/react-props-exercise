303d8740ebd190305ec55a45622b3763b22bfb8d
Jessica Bryant <jessica.bryant@live.com>
2021-11-16 10:03:41 -0500

    Add Bonus to Lesson (missing a .)

--- src/exercise/Exercise.js
+++ src/exercise/Exercise.js
@@ -1,6 +1,7 @@
 // REACT PROPS EXERCISE
 // 6c.
-// Cannot figure 6c. out. I think it will involve the props in some way to pass the Bonus Function Component through Exercise Function Component, but I can't figure out how to do it. I'm sure it will make sense when gone over in class. I'm reading an article online from pretagteam.com that says you cannot pass state between sibling components, but a solution will be to wrap both siblings in a parent component and handle the state inside the parent component and then share between siblings, but I don't quite get this. May come back to it, but for now I am taking a break.
+import Bonus from "../bonus/Bonus";
+// TWO DOTS TO GO UP TWICE!!! THAT's ALL I HAD WRONG.
 
 // 1a., 3b., and 3d.
 function Exercise({good="Great", bad="Not Great"}){ 
@@ -10,6 +11,7 @@ function Exercise({good="Great", bad="Not Great"}){
             {/* 3c. */}
             <h1>{good}</h1>
             <h1>{bad}</h1>
+            <Bonus />
         </div>
     );
 }
